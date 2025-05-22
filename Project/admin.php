63f8990ada0d677898ab45d347b8def883d5d5ea
amarlicious <amarkhalid058@gmail.com>
2025-05-22 09:47:04 +0800

    updet

--- Project/admin.php
+++ Project/admin.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $name="Uncle mutu";
+    $type="hensem";
 
+    echo "Nama saya $name<br>";
+    echo "saya sangat $type";
+
+    ?>
+</body>
+</html>
