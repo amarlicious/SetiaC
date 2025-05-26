da198b166bf5575bd991f22a244425bd1d3f7c7a
amarlicious <amarkhalid058@gmail.com>
2025-05-26 15:39:19 +0800

    update

--- adminfile/admin.php
+++ adminfile/admin.php
@@ -5,19 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
     
-    <!-- Link to your CSS file inside the css folder -->
     <link rel="stylesheet" href="../css/admin.css" type="text/css">
 </head>
 <body>
-    <nav>
-        <p id="admin">Admin</p>
+    <div class="main-wrapper"> <div class="admin-sidebar" >
+            <p class="admin">Admin</p>
+            <ul>
+                <li id="Home">Home</li>
+                <li id="Report">Report</li>
+                <li id="message">Message</li>
+                <li id="setting"><a href="../Project/setting.php">Setting</a></li>
+            </ul>
+        </div>
 
-        <ul>
-            <li id="Home">Home</li>
-            <li id="Report">Report</li>
-            <li id="message">Message</li>
-            <li id="setting"><a href="../Project/setting.php">Setting</a></li>
-        </ul>
-    </nav>
-</body>
+        <div class="side-admin">
+            <p>woi</p>
+            </div>
+    </div> </body>
 </html>
