70f338ac3809ec88eeedb39861d66ad13d8fbb7f
amarlicious <amarkhalid058@gmail.com>
2025-05-28 21:48:17 +0800

    reprto

---
+++ adminfile/report.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Report</title>
+    
+     <link rel="stylesheet" href="../css/report.css" type="text/css">
+</head>
+
+<body>
+    <div class="head">
+        
+        <h1>Report</h1>
+    </div>
+</body>
+</html>
