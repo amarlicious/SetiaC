22366c38e4c2967b805481160a2de9d185a5b835
amarlicious <amarkhalid058@gmail.com>
2025-05-28 22:00:03 +0800

    update

--- adminfile/report.php
+++ adminfile/report.php
@@ -1,17 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Report</title>
-    
-     <link rel="stylesheet" href="../css/report.css" type="text/css">
-</head>
-
-<body>
-    <div class="head">
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Report</title>
         
-        <h1>Report</h1>
-    </div>
-</body>
-</html>
+        <link rel="stylesheet" href="../css/report.css" type="text/css">
+    </head>
+
+    <body>
+        <div class="head">
+            
+            <h1>Report</h1>
+        </div>
+
+        <div class="container">
+            <img class="camera" src="camera.jpeg" alt="camera icon">
+        </div>
+    </body>
+    </html>
