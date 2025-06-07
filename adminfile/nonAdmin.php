925e5d67023a6bb75e98356172f9646008b6af76
amarlicious <amarkhalid058@gmail.com>
2025-06-07 22:50:34 +0800

    non admin

---
+++ adminfile/nonAdmin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Non Admin
+  <link rel="stylesheet" href="../css/nonAdmin.css" type="text/css"/>
+</head>
+
+<body>
+  <div class="head">
+    <h1>Report</h1>
+  </div>
+
+  <div class="main-content">
+    <div class="bulatan">
+      <img class="icon" src="no icon.jpeg" alt="no Icon" />
+    </div>
+
+    <div class="text"><P>Sorry Only Admin Can Access This Feature</P></div>
+
+
+</div>
+
+<div>
+     <button type="submit" class="home-button"><a href="../Project/home.php">Home</a></button>
+
+</div>
+  
+</body>
+</html>
