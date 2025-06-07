8f4e2b2123b2f1df42a3ad8555302c7820cc13b2
amarlicious <amarkhalid058@gmail.com>
2025-06-08 01:42:58 +0800

    update

--- adminfile/nonAdmin.php
+++ adminfile/nonAdmin.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Non Admin
+  <title>Non Admin</title>
   <link rel="stylesheet" href="../css/nonAdmin.css" type="text/css"/>
 </head>
 
