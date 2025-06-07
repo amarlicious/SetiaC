07389551dcb0938502471dc9207f56c78da6e5d3
amarlicious <amarkhalid058@gmail.com>
2025-06-07 22:40:03 +0800

    submit report

---
+++ adminfile/reportSubmit.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Done Submit</title>
+  <link rel="stylesheet" href="../css/reportSubmit.css" type="text/css"/>
+</head>
+
+<body>
+  <div class="head">
+    <h1>Report</h1>
+  </div>
+
+  <div class="main-content">
+    <div class="bulatan">
+      <img class="icon" src="right icon.jpg" alt="Success Icon" />
+    </div>
+
+    <div class="text"><P>Report Submitted!</P></div>
+
+    <button type="submit" class="home-button"><a href="../Project/home.php">Home</a></button>
+  </div>
+
+
+
+  
+</body>
+</html>
