3f30da735f5722ae7e89755ed9b1f40af6afa9c0
amarlicious <amarkhalid058@gmail.com>
2025-06-15 20:11:51 +0800

    up

--- adminfile/reportSubmit.php
+++ adminfile/reportSubmit.php
@@ -1,18 +1,3 @@
-<?php
-session_start();
-include('connection.php');
-
-if (!isset($_SESSION['last_report_id'])) {
-    echo "No report found.";
-    echo "<meta http-equiv='refresh' content='3; URL=report.php'>";
-    exit();
-}
-
-$report_id = $_SESSION['last_report_id'];
-$sql = "SELECT * FROM report WHERE id = $report_id";
-$result = $conn->query($sql);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,23 +22,5 @@ $result = $conn->query($sql);
     <a href="../Project/home.php"><button class="home-button">Home</button></a>
   </div>
 
-  <section style="margin-top: 40px;">
-    <?php
-    if ($result && $result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        echo "<center><h3>YOUR REPORT</h3></center>";
-        echo "<table align='center' border='1' width='50%'>";
-        echo "<tr><th>Text</th><td>" . htmlspecialchars($row['report_text']) . "</td></tr>";
-        echo "<tr><th>Category</th><td>" . htmlspecialchars($row['category']) . "</td></tr>";
-
-        $img = (!empty($row['picture']) && file_exists($row['picture'])) ? $row['picture'] : 'default.png';
-        echo "<tr><th>Picture</th><td><img src='" . htmlspecialchars($img) . "' width='150' /></td></tr>";
-        echo "</table>";
-    } else {
-        echo "<p style='text-align: center;'>Report not found.</p>";
-    }
-    $conn->close();
-    ?>
-  </section>
 </body>
 </html>
