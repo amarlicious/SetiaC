e8ef4b3812b5546777eaf3c8402a5188f3e54d44
Your Name <you@example.com>
2025-06-23 12:12:07 +0800

    buang adminreport

--- admin.php
+++ admin.php
@@ -57,7 +57,6 @@ if ($stmt = $conn->prepare($sql)) {
         <ul>
             <li id="Home"><a href="main.php">Home</a></li>
             <li id="residence"><a href="residenceinfo.php">Residence's Information</a></li>
-            <li id="Report"><a href="report.php">Report</a></li>
             <li id="feedback"><a href="feedbackadmin.php">Feedback</a></li>
             <li id="setting"><a href="statusUpdate.php">Status Update</a></li>
 
